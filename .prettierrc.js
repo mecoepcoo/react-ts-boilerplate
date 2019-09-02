d72d14778215ed77bfbda9372c0fe8683c31b33a
tianzhen <340349237@qq.com>
2019-09-02 11:56:18 +0800

    test: husky & lint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "printWidth": 160, //一行的字符数，如果超过会进行换行，默认为80
+  "printWidth": 120, //一行的字符数，如果超过会进行换行，默认为80
   "tabWidth": 2,
-  "useTabs": false,
+  "useTabs": false, // 注意：makefile文件必须使用tab
   "singleQuote": true,
   "semi": true,
   "trailingComma": "none", //是否使用尾逗号，有三个可选值"<none|es5|all>"
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from './logo.svg';
-import './App.less';
+import './App.less'
 
 const App: React.FC = () => {
   return (
