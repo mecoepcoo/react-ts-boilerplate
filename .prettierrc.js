b4d24100645df71d8d7c0b570d72addbbae4cf30
tianzhen <340349237@qq.com>
2019-09-02 11:11:33 +0800

    fix: 修复prettierrc文件名错误

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,5 +6,4 @@ module.exports = {
   "semi": true,
   "trailingComma": "none", //是否使用尾逗号，有三个可选值"<none|es5|all>"
   "bracketSpacing": true, //对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
-  "parser": "babylon" //代码的解析引擎，默认为babylon，与babel相同。
 };
